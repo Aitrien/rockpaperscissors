352fe2e80a6eaf270a6f305b2ddf0ce5473fd05e
Aitrien <aitriens@gmail.com>
2021-09-16 12:26:40 +1200

    user interface implemented

--- rockpaper.js
+++ rockpaper.js
@@ -1,3 +1,15 @@
+const btnRock = document.querySelector('#btn1');
+const btnPaper = document.querySelector('#btn2');
+const btnScissors = document.querySelector('#btn3');
+const body = document.querySelector('body');
+
+const output = document.querySelector('#output');
+const score = document.querySelector('#score');
+
+let turnsPlayed = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * 3); // random integer from 0 to 3
@@ -11,16 +23,24 @@ function playRound(playerInput, computerMove) {
     if (playerMove === computerMove) {
         let message = playerMove.toUpperCase() + " and " + computerMove.toUpperCase();
         console.log(message + "? It's a draw!");
+        output.textContent = message + "? It's a draw!";
+        body.style.backgroundColor = "paleturquoise";
         return "draw";
     }
     else if (playerMove === "rock" && computerMove === "scissors" || playerMove === "paper" && computerMove === "rock" || playerMove === "scissors" && computerMove === "paper") {
         let message = playerMove.toUpperCase() + " beats " + computerMove.toUpperCase();
         console.log(message + ". You win!");
+        output.textContent = message + ". You win!";
+        body.style.backgroundColor = "palegreen";
+        playerWins += 1;
         return "player";
     }
     else {
         let message = computerMove.toUpperCase() + " beats " + playerMove.toUpperCase();
         console.log(message + ". You lose!");
+        output.textContent = message + ". You lose!";
+        body.style.backgroundColor = "palevioletred";
+        computerWins += 1;
         return "computer";
     }
 }
@@ -61,6 +81,45 @@ function game(turns) {
         console.log("Wow! A draw!");
     }
 }
+
+
+function updateScore() {
+  if (turnsPlayed > 5) turnsPlayed = 1;
+  if (turnsPlayed === 5) {
+    if (playerWins > computerWins) {
+      score.textContent = "Game won!" + "\n" + playerWins + " / " + turnsPlayed;
+    }
+    else if (playerWins < computerWins) {
+      score.textContent = "Game lost!" + "\n" + playerWins + " / " + turnsPlayed;
+    }
+    else {
+      score.textContent = "Wow! A draw!" + "\n" + playerWins + " / " + turnsPlayed;
+    }
+    playerWins = 0;
+    computerWins = 0;
+  }
+  else {
+    score.textContent = playerWins + " / " + turnsPlayed;
+  }
+}
+
+btnRock.addEventListener('click', () => {
+  outcome = playRound('rock', computerPlay());
+  turnsPlayed += 1;
+  updateScore();
+});
+
+btnPaper.addEventListener('click', () => {
+  outcome = playRound('paper', computerPlay());
+  turnsPlayed += 1;
+  updateScore();
+});
+
+btnScissors.addEventListener('click', () => {
+  outcome = playRound('scissors', computerPlay());
+  turnsPlayed += 1;
+  updateScore();
+});
 
 //const playerSelection = "rock";
 //const computerSelection = computerPlay();
